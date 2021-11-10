59a58b5ada88fcbc986f378886a807bc6801c061
falcon758 <falcon758@users.noreply.github.com>
2021-11-10 00:18:52 +0000

    Update bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -6,7 +6,7 @@
  *
  * @category Api
  * @package  Symphony_Api
- * @author   Luis Campos <luis.campos@log.pt>
+ * @author   User <user@log.pt>
  * @license  MIT License (c) copyright 2011-2013 original author or authors
  * @link     https://github.com/falcon758/symphony_api
  */
